1a78df6851329ffc3ae27913676b74f509616474
yogeshdhangar5213 <yogeshdhangar5213@gmail.com>
2022-10-11 03:39:28 -0700

    Added: Welcome To Gambling Game

---
+++ src/main/java/Gambling.java
@@ -0,0 +1,25 @@
+import java.util.Random;
+
+public class Gambling {
+    public static void main(String[] args) {
+        int money = 100;
+        int playCount = 0;
+        int wonCount = 0;
+        Random random = new Random();
+        while(money!=200&&money!=0){
+            playCount++;
+            System.out.println("Initial Money:"+money);
+            int bet = random.nextInt(2);
+            if(bet==1){
+                money = money+1;
+                wonCount++;
+            }else{
+                money = money-1;
+                System.out.println("Remaining Money Is :"+money);
+            }
+        }
+        System.out.println("No of times Played:"+playCount);
+        System.out.println("No Of Times Won:"+wonCount);
+        System.out.println("No Of Times Lost:"+(playCount-wonCount));
+    }
+}
